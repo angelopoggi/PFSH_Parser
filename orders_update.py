770402c609e8af8c5c439409dae045db2a5584bb
Angelo Poggi <angelo.poggi@networktocode.com>
2024-04-30 08:22:36 -0400

    Logic to check if orders file is created

--- orders_update.py
+++ orders_update.py
@@ -13,6 +13,7 @@ from pfsh_parser.creds import (
 )
 
 import time
+import os
 
 username = PFSH_USERNAME
 password = PFSH_PASSWORD
@@ -23,13 +24,17 @@ logger.log(f"Fetching Orders from Shopify API")
 order_parser(SHOP_NAME, "open", SHOPIFY_ACCESS_TOKEN)
 logger.log(f"PUSH MODIFIED ORDERS FILE TO SFTP")
 # push new orders
-time.sleep(1)
-sftp_connect(
-    host=host,
-    port=22,
-    username=username,
-    password=password,
-    direction="push",
-    local_file=f"files/tmp/{UPDATED_ORDERS_FILE}",
-    remote_file=f"Orders/POSTFORDERS.csv",
-)
+if os.path.exists(f"files/tmp/{UPDATED_ORDERS_FILE}"):
+    print("Update orders file was found - pushing to FTP")
+    time.sleep(1)
+    sftp_connect(
+        host=host,
+        port=22,
+        username=username,
+        password=password,
+        direction="push",
+        local_file=f"files/tmp/{UPDATED_ORDERS_FILE}",
+        remote_file=f"Orders/POSTFORDERS.csv",
+    )
+else:
+    print("No Orders file found - skipping")
